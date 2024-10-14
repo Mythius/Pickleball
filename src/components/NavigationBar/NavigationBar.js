45a8d3dda115b9331586e1d7a8d476b527c80acf
matthiassouthwick <southwickmatthias@gmail.com>
2024-10-14 13:30:02 -0600

    Added Login Logic

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -3,22 +3,26 @@ import PropTypes from "prop-types";
 import menuBar from "./3lines.svg";
 import profile from "./profile.png";
 import "./NavigationBar.css";
-import loadProfile from '../googleSignIn/autoLogin';
+import loadProfile from "../googleSignIn/autoLogin";
 
 const NavigationBar = () => {
-
-
   const toggleMenu = () => {
-    document.querySelector('nav')?.classList.remove('closed');
+    document.querySelector("nav")?.classList.remove("closed");
   };
 
   loadProfile();
 
   return (
     <div className="topbar">
-      <div className="logo">
-        <img className="icon" src={menuBar} alt="" onClick={toggleMenu} />
-      </div>
+      {
+        (window.location.pathname == "/" ? (
+          ""
+        ) : (
+          <div className="logo">
+            <img className="icon" src={menuBar} alt="" onClick={toggleMenu} />
+          </div>
+        ))
+      }
       <h1 className="title">Pickleball App</h1>
       <div className="userProfile">
         <img className="icon" src={profile} alt="" />
